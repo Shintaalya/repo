b155d3f87225d64d46b70cb33b6051c5450b2223
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-21 16:36:57 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -104,7 +104,7 @@ elif choose=='Predict':
         return denormalized_data
     
     # Streamlit app code
-    def main():
+     def main():
         st.title('Prediksi Harga Rumah')
     
         # Input form
